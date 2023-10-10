4fee8e1544db779561268bdf7f83c079169d7a4b
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-10 20:41:15 +0300

    Add files via upload

---
+++ pr7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Практическая работа №7");
+            Console.WriteLine("Здравствуйте!");
+            Console.WriteLine("Введите количество чисел: "); // вводим значение N 
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = 0; // переменная для хранения суммы квадратов чисел
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Введите число {i + 1}: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                a += num * num;
+            }
+            double b = Math.Sqrt(a / N); // вычисляем среднее квадратическое
+            Console.WriteLine($"Среднее квадратическое N чисел: {b}");
+        }
+    }
+}
