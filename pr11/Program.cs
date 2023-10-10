d49ff4e64bb7dcf2b55ffaa75abd50b32d8201c7
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-10 10:49:13 +0300

    Add files via upload

---
+++ pr11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Практическая работа №11");
+            Console.WriteLine("Здравствуйте!");
+            try
+            {
+                string S = null;
+                int index = 0;
+                Console.WriteLine("Введите, пожалуйста, текст: ");
+                S = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(S)) // проверка строки
+                {
+                    Console.WriteLine("Строка пустая! Вы ничего не ввели.");
+                    Console.ReadKey();
+                    Environment.Exit(0); // досрочный выход из программы
+                }
+
+                int a = S.Length; // длина строки
+
+            }
+            
+            catch(Exception ex) // обработка общего исключения
+            { 
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Что-то пошло не так. Ошибка: " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
