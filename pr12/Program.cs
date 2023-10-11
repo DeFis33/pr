fb4c0031200d37d76f154f83e36d919255aa25d5
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-11 09:43:35 +0300

    Add files via upload

---
+++ pr12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Практическая работа №12");
+            Console.WriteLine("Здравствуйте!");
+
+            try
+            {
+                Console.Write("Введите предложение: ");
+                System.String predl = Console.ReadLine();
+                
+                Console.Write("Введите три буквы, которые вы хотели бы заменить: ");
+                System.String a = Console.ReadLine();
+                
+                Console.Write("Введите три буквы, которые вы хотели бы использовать для замены: ");
+                System.String b = Console.ReadLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+// Contains: определяет, содержится ли подстрока в строке
+// Replace: замещает в строке символ или подстроку другим символом или подстрокой
