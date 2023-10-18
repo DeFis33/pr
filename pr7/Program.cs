bc7557141b6331b2cb4dc39b08b0083b1355e2b2
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-18 15:48:38 +0300

    Update Program.cs

--- pr7/Program.cs
+++ pr7/Program.cs
@@ -16,55 +16,59 @@ namespace pr7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Практическая работа №7");
-            Console.WriteLine("Здравствуйте!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Практическая работа №7 \nЗдравствуйте!");
 
             while (true)
             {
-                Console.WriteLine("Введите количество чисел (для завершения введите 0): ");// вводим значение N
                 try
                 {
                     try
                     {
+                        Console.WriteLine("Введите количество чисел (для завершения введите 0): "); // вводим значение N
                         int N = Convert.ToInt32(Console.ReadLine());
 
-                        if (N == 0)
+                        if (N == 0) // если N = 0, то завершаем программу
                         {
                             Console.WriteLine("Программа завершена.");
                             break;
                         }
-                        else if (N < 0)
+                        else if (N < 0) // если N < 0, то просим пользователя ввести положительное число
                         {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine("Пожалуйста, введите положительное целое число.");
+                            Console.ForegroundColor = ConsoleColor.White;
                             continue;
                         }
 
-                        int a = 0; // переменная для хранения суммы квадратов чисел
+                        int a = 0;
                         for (int i = 0; i < N; i++)
                         {
                             Console.Write($"Введите число {i + 1}: ");
-                            int num = Convert.ToInt32(Console.ReadLine());
-                            a += num * num;
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            // нужно сохранить каждое введённое число пользователя, для вычисления квадрата расстояния... (2 этап)
+                            a += number; 
                         }
+                        int c = a / N; // вычисляем среднее арифметическое (1 этап)
 
-                        double b = Math.Sqrt(a / N); // вычисляем среднее квадратическое
+                        //double b = Math.Sqrt(a / N); заменить a на другую переменнную, т.к. она занята (3 этап)
                         Console.WriteLine($"Среднее квадратическое N чисел: {b}");
+
                     }
-                    catch (FormatException fe)
+                    catch (FormatException fe) // частное исключение
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"Ошибка ввода \n" + fe.Message);
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
-                catch (Exception e)
+                catch (Exception e) // общее исключение
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Ошибка ввода \n" + e.Message);
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
             }
-            
             Console.WriteLine("До свидания!");
             Console.ReadKey();
         }
