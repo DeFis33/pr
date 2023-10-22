5b28310b5133c15604f3e68b0443cf045fc388a9
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-22 23:25:20 +0300

    Update Program.cs

--- pr8/Program.cs
+++ pr8/Program.cs
@@ -17,29 +17,41 @@ namespace pr8
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Практическая работа №8. \n Здравствуйте! ");
+            Console.WriteLine("Практическая работа №8. \nЗдравствуйте! ");
 
             while (true)
             {
                 try
                 {
-                    try
-                    {
-                        Console.WriteLine("... (для завершения введите 0): ");
-                        int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите любую цифру, чтобы увидеть таблицу значений функции y=5-x^2/2 на отрезке [-5; 5] с шагом 0,5. \n(для завершения введите 0): ");
+                    int a = Convert.ToInt32(Console.ReadLine());
 
-                        if (a == 0) // если a = 0, то завершаем программу
-                        {
-                            Console.WriteLine("Программа завершена.");
-                            break;
-                        }
-                    }
-                    catch (FormatException fe) // частное исключение
+                    if (a == 0) // если a = 0, то завершаем программу
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Ошибка ввода \n" + fe.Message); // вывод ошибки на экран
-                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Программа завершена.");
+                        break;
                     }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("    x |     y");
+                    Console.WriteLine("----------------");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    double x = -5, end = 5, step = 0.5;
+
+                    do
+                    {
+                        double y = 5 - Math.Pow(x, 2) / 2; // вычисление значения функции для текущего значения x
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($" {x,4} | {y,6}"); // вывод значения x и y на консоль
+                        Console.ForegroundColor = ConsoleColor.White;
+                        x += step; // увеличение значения x на шаг (step)
+                    } while (x <= end); // выполнение цикла do while до тех пор, пока x не станет больше end
+                }
+                catch (FormatException e) // частное исключение
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка ввода \n" + e.Message); // вывод ошибки на экран
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                 catch (Exception e) // общее исключение
                 {
@@ -48,7 +60,7 @@ namespace pr8
                     Console.ForegroundColor = ConsoleColor.White;
                 }
             }
-            Console.ReadKey();
+            Console.WriteLine("До свидания!");
         }
     }
 }
