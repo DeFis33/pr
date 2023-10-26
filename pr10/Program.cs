6e2d3a00bb09a8ef9d093ff74ada275eb8805f38
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-10-26 14:50:49 +0300

    Update Program.cs

--- pr10/Program.cs
+++ pr10/Program.cs
@@ -4,72 +4,97 @@
 //* Задание: обработать двухмерный массив                       *
 //***************************************************************
 
+using System;
+using System.Net;
+
 namespace pr10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            try
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Практическая работа №10.\nЗдравствуйте!\n");
+
+            while (true)
             {
-                int M = 3, N = 5;
-                Random rnd = new Random(); // создание экземпляра генератора случайных чисел
-                int[,] matrix = new int[M, N]; // объявление и инициализация двумерного массива
-                int product = 1; // product - хранение произведения ненулевых элементов матрицы
-                Console.WriteLine("Практическая работа №10.\nЗдравствуйте!\n");
-                
-                Console.WriteLine("Если ВЫ хотите сами ввести элементы матрицы, то введите цифру 1. Иначе 0.");
-
-
-                if () // если пользователь хочет сам ввести элементы матрицы, то
+                try
                 {
-                    Console.WriteLine("Введите элементы матрицы:");
-                    for (int i = 0; i < M; i++) // ввод по строкам
-                    {
-                        for (int j = 0; j < N; j++) // ввод по столбцам
-                        {
-                            Console.Write("Элемент [" + i + ", " + j + "]: ");
-                            matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    int M = 3, N = 5;
+                    Random rnd = new Random(); // создание экземпляра генератора случайных чисел
+                    int[,] matrix = new int[M, N]; // объявление и инициализация двумерного массива
+                    int product = 1; // product - хранение произведения ненулевых элементов матрицы
 
-                            if (matrix[i, j] != 0) // если matrix[i, j] не равно нулю, то
-                            {
-                                product *= matrix[i, j]; // умножение текущего значения product на значение ненулевого элемента, накапливая произведение ненулевых элементов
-                            }
-                        }
-                        Console.WriteLine();
-                    }
-                }
-                else () // иначе
-                {
-                    for (int i = 0; i < M; i++)
+                    Console.WriteLine("Хотите ли вы сами ввести элементы матрицы? (Да/Нет).\nЛибо введите команду (Выход), которая завершит работу программы:");
+                    string a = Console.ReadLine();
+                    Console.WriteLine("");
+
+                    if (a == "Выход")
                     {
-                        for (int j = 0; j < N; j++)
-                        {
-                            matrix[i, j] = rnd.Next(-100, 101); // генерация неповторяющихся значений
-                            Console.Write(matrix[i, j] + "\t");
-                            if (matrix[i, j] != 0)
-                            {
-                                product *= matrix[i, j];
-                            }
-                        }
-                        Console.WriteLine();
+                        Console.WriteLine("Программа завершена.\nДо свидания!");
+                        break;
                     }
+                    if (a == "Да") // если пользователь хочет сам ввести элементы матрицы, то
+                    {
+                        Console.WriteLine("Введите элементы матрицы:");
+                        for (int i = 0; i < M; i++) // ввод по строкам
+                        {
+                            for (int j = 0; j < N; j++) // ввод по столбцам
+                            {
+                                Console.Write("Элемент [" + i + ", " + j + "]: ");
+                                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                                if (matrix[i, j] != 0) // если matrix[i, j] не равно нулю, то
+                                {
+                                    product *= matrix[i, j]; // умножение текущего значения product на значение ненулевого элемента, накапливая произведение ненулевых элементов
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else if (a == "Нет") // иначе
+                    {
+                        for (int i = 0; i < M; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                matrix[i, j] = rnd.Next(-100, 101); // генерация неповторяющихся значений
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(matrix[i, j] + "\t");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                if (matrix[i, j] != 0)
+                                {
+                                    product *= matrix[i, j];
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Введите Да или Нет!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("\nПроизведение ненулевых элементов: " + product);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("");
                 }
-                Console.WriteLine("Произведение ненулевых элементов: " + product);
+                catch (FormatException e) // частное исключение
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка ввода \n" + e.Message); // вывод ошибки на экран
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                catch (Exception e) // общее исключение
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка ввода \n" + e.Message); // вывод ошибки на экран
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
             }
-            catch (FormatException e) // частное исключение
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Ошибка ввода \n" + e.Message); // вывод ошибки на экран
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            catch (Exception e) // общее исключение
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Ошибка ввода \n" + e.Message); // вывод ошибки на экран
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            Console.WriteLine("\nДо свидания!");
             Console.ReadKey();
         }
     }
